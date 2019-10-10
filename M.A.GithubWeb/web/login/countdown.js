b197c26f3f6407ac8385050b4dd58bcf945bfbaf
Sahar Hagbi <noamsahar17@gmail.com>
2019-10-10 21:56:34 +0300

    sort html files to directories, and starting repository html page

--- M.A.GithubWeb/web/login/countdown.js
+++ M.A.GithubWeb/web/login/countdown.js
@@ -2,14 +2,11 @@ var refreshRate = 2000; //milli seconds
 var counter = 0;
 
 function counting() {
-/*$("#counter").
-counter ++;*/
+    /*$("#counter").
+    counter ++;*/
 }
 
 $(function () {
-
     //The users list is refreshed automatically every second
     setInterval(counting, refreshRate);
-
-
 });
